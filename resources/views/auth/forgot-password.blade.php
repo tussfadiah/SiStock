a5009ecb04af12825bae2333f19f4fb28e852b22
tussfadiah <tussholihafadiah@gmail.com>
2026-07-20 10:09:57 +0700

    final website

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -10,9 +10,12 @@
         @csrf
 
         <!-- Email Address -->
+          
+
         <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+            <x-input-label for="email" :value="__('Email')" class="block text-sm font-semibold text-gray-700" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus    class="block mt-1 w-full px-3 py-2 bg-white border border-gray-400 rounded-md text-sm shadow-sm placeholder-gray-400
+                          focus:outline-none focus:border-[#000000] focus:ring-1 focus:ring-[#000000]"  />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
